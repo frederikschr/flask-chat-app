e1d8b220ec5346993ca4c5a34ee22db99592184f
Frederik Schier <fredi.schier@gmail.com>
2021-08-22 14:00:02 +0200

    update

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from app import create_app
 socketio, app = create_app()
 
 if __name__ == '__main__':
-    #socketio.run(app, debug=True)
     with app.app_context():
         socketio.run(app, debug=True)
 
